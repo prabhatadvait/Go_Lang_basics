ce5c6f85025d322f13cbed205f379a40ea438b83
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-04-28 17:24:18 +0530

    Add files via upload

---
+++ awesomeProject/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var shortGolang = "Watch more courses."
+	var fullGolang = "Watch prabhat Go Crash Course"
+	var rewardPhone = "Reward myself with a Phone."
+
+	fmt.Println("#### Welcome to Todolist App! ####")
+
+	fmt.Println("List of my Todos")
+	fmt.Println(fullGolang)
+	fmt.Println(shortGolang)
+	fmt.Println(rewardPhone)
+
+	fmt.Println()
+	fmt.Println("Tutorials")
+	fmt.Println(fullGolang)
+	fmt.Println(shortGolang)
+
+	fmt.Println()
+	fmt.Println("Rewards")
+	fmt.Println(rewardPhone)
+
+	fmt.Println()
+	fmt.Println("Projects ")
+	fmt.Println(shortGolang)
+
+}
